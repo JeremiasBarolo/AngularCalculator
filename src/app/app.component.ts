db03f0d62cd6aeab2d7a63f61909641583b47055
Jeremias Barolo <barolojeremias@gmail.com>
2023-06-29 19:04:07 -0300

    Creation of Angular Calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'mini-calculator';
+  title = 'Calculator Using Angular';
+
+  num1: number = 0;
+  num2: number = 0;
+  
+  res: number = 0;
+
+  add():void{
+    this.res = this.num1 + this.num2;
+    
+  }
+  subtract():void{
+    this.res = this.num1 - this.num2;
+    
+  }
+  multiply():void{
+    this.res = this.num1 * this.num2;
+    
+  }
+  divide():void{
+    this.res = this.num1 / this.num2;
+    
+  }
 }
